1b652270050eccfeda56305dc166a79dd3fb0027
Carlos Toro <carlos_toro@gap.com>
2023-01-17 21:21:54 -0500

    Updated hellow path to healty path

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ const Express = require("express")
 const app = new Express()
 const PORT = 3000
 
-app.get('/hello', (req, res) => {
-    console.log(req)
-    return res.send('<h2>Hola Puerco</h2>')
+app.get('/healty', (req, res) => {
+    return res.status(200).json({ 
+        status: "Running",
+        version: '0.0.1'
+     })
 })
 
 app.listen(PORT, () => {
