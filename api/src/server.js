924c04f01395f6ab6b07d9a409ca967c0d06882e
Eclyp <hectoraderfer123421@gmail.com>
2023-02-11 20:54:03 -0500

    Mongodb is added this project

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,30 @@
 import Express from 'express'
 
 import routes from './routes.js'
+import mongoose from 'mongoose'
 
 const app = new Express()
 const PORT = 3000
+const { Schema } = mongoose;
 
-app.use(routes)
+const uri = 'mongodb://127.0.0.1:27017/test'
+mongoose.set('strictQuery', true);
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(uri, () => {
+    console.log("Connected to MongoDB");
+  });
+}
 
+const schusers = new Schema({
+    name: String
+});
+const usermodel = mongoose.model('usermodel', schusers);
+const nameuser = new usermodel({ name: 'Username'})
+console.log(nameuser.name)
+
+
+app.use(routes);
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`)
 })
